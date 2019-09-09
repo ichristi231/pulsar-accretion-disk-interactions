ccb8b66cb58063b5cf0100e3949bb996c4dbc4ee
Ian Christie <ichristi231@gmail.com>
2019-09-09 16:10:09 -0500

    added features for computing the particle distribution and synchrotron spectrum beyond the pericenter time

--- pulsar_IR_flare_test.py
+++ pulsar_IR_flare_test.py
@@ -30,7 +30,7 @@ electric_charge = 4.8032068 * (10**-10.)
 ##########################################################
 
 # Spin-down luminosity of the pulsar in erg/seconds.
-spin_down_L = 3.*10**35.
+spin_down_L = 3. * 10**35.
 
 # Slope of injected particle distribution (assumed to be a power-law).
 particle_slope = 2.2
@@ -39,33 +39,42 @@ particle_slope = 2.2
 epsilon_e = 0.1
 epsilon_B = 0.1
 
+# Particle number density, in cm^-3, at the Bondi radius.
+Bondi_num_den = 10**2
+
+# Minimum Lorentz factor of the injected particle distribution.
+gamma_min = 10**3.
+
 # Pericenter distance of the pulsar and Bondi Radius in cm.
-pericenter_dist = 5.*10**(16.)
+pericenter_dist = 5. * 10**(16.)
 Bondi_radius = 10**17.
+
+# Alpha-viscosity parameter of the accretion disk (see for more
+# details). Used in estimating the accretion timescale (see eqn.
+# 3 in Christie et al. 2017).
+alpha_viscosity = 0.01
 
 # Pericenter time, i.e. the time it takes the pulsar to traverse
 # through the accretion disk, in s.
 pericenter_time = 3 * (10**7.) * (pericenter_dist / (10**16.))**(3. / 2.)
 
+# Accretion timescale in seconds.
+accretion_time = 600 * pericenter_time * (alpha_viscosity / 0.01)**(-1)
+
 # Time, in seconds, from when the pulsar first intercepts the accretion disk to
 # the pericenter time.
-timestable = np.array([10**-6, 10**-4, 10**-2, 1]) * pericenter_time
-
-# Particle number density, in cm^-3, at the Bondi radius.
-Bondi_num_den = 10**2
+timestable = np.array([10**-6, 10**-4, 10**-2, 1, 10, 50, 100, accretion_time /
+    pericenter_time]) * pericenter_time
 
 # Magnetic field strength at the pericenter distance in Gauss.
 # Note that we follow the parametric scalings provided in the
 # Christie et al. 2017.
-pericenter_mag_field = 0.007 * np.sqrt((epsilon_B / 0.1) * (Bondi_num_den / 100.) * (Bondi_radius / (10**17.))) * (pericenter_dist / (10**16.))**(-1)
-# print("Magnetic field strength: " + str(pericenter_mag_field) + " G. \n")
+pericenter_mag_field = 0.007 * np.sqrt((epsilon_B / 0.1) * (Bondi_num_den /
+    100.) * (Bondi_radius / (10**17.))) * (pericenter_dist / (10**16.))**(-1)
 
 
-# Minimum Lorentz factor of the injected particle distribution.
-gamma_min = 10**3.
-
-print("\nPericenter time is " + str(pericenter_time) + " s = " + str(pericenter_time / 3600.) + " hr.\n")
-
+print("\nPericenter time is " + str(pericenter_time) + " s = " + str(
+    pericenter_time / 3600.) + " hr.\n")
 ##########################################################
 ##########################################################
 
@@ -76,7 +85,8 @@ print("\nPericenter time is " + str(pericenter_time) + " s = " + str(pericenter_
 # Maximum Lorentz factor of particle distribution assuming a Bohm
 # acceleration.
 gamma_max = 1.4 * (10**9.) * ((epsilon_B / 0.1) * (Bondi_num_den / 100.) *
-	(Bondi_radius / (10**17.)))**(-1. / 4.) * (pericenter_dist / (10**16.))**(1. / 2.)
+	(Bondi_radius / (10**17.)))**(-1. / 4.) * (pericenter_dist / (10**16.))**(1. /
+    2.)
 
 # Table of Lorentz factors ranging from 1 to gamma_max.
 gamma_table = np.logspace(0, np.log10(gamma_max), 99)
@@ -87,49 +97,72 @@ particle_distribution_norm = epsilon_e * spin_down_L * (particle_slope - 2) / (
         gamma_max**(2 - particle_slope)))
 
 # Factor b defined just after eqn. 5 in the paper.
-b = pericenter_mag_field**2 * thomson_cross_section / (6 * np.pi * electron_mass * speed_light)
+b = pericenter_mag_field**2 * thomson_cross_section / (6 * np.pi *
+    electron_mass * speed_light)
 
 # Cooling Lorentz factors gc1(t) and gc2(t), defined following eqn. 8 in
-# Christie et al. 2016.
+# Christie et al. 2017.
 gamma_cool_1 = gamma_max / (1 + timestable * b * gamma_max)
 gamma_cool_2 = gamma_min / (1 + b * timestable * gamma_min)
 
 # Initialize particle distribution.
-particle_distribution = np.zeros((np.shape(timestable)[0], np.shape(gamma_table)[0]))
+particle_distribution = np.zeros((np.shape(timestable)[0], np.shape(gamma_table
+    )[0]))
 # Initialize an array which stores the different components of the
-# piecewise function provided in eqn. 8.
+# piecewise function provided in eqn. 8 of Christie et al. 2017.
 f = 0 * gamma_table
 
-# Below, we go through each of the four parts of the piecewise function,
-# provided in eqn. 8, and add it to the existing particle distibution. Note
-# that they are listed in the same order as eqn. 8.
+# For each time value provided in the timestable, we compute
+# the particle distribution at that particular step.
 for i in range(np.shape(timestable)[0]):
-    f = (gamma_table > gamma_min) * (gamma_table < gamma_cool_1[i]) * (1 - (1 -
-    b * gamma_table * timestable[i])**(particle_slope - 1))
-    f[np.isnan(f)] = 0
-    particle_distribution[i] += f
+    # Below, we go through each of the four parts of the piecewise function,
+    # provided in eqn. 8, and add it to the existing particle distibution. Note
+    # that they are listed in the same order as eqn. 8 and that this if-section
+    # corresponds to times below the pericenter time.
+    if timestable[i] <= pericenter_time:
+        f = (gamma_table > gamma_min) * (gamma_table < gamma_cool_1[i]) * (1 -
+            (1 - b * gamma_table * timestable[i])**(particle_slope - 1))
+        f[np.isnan(f)] = 0
+        particle_distribution[i] += f
 
-    f = 0 * gamma_table
-    f = (gamma_cool_1[i] < gamma_table) * (gamma_table < gamma_max) * (1 -
-        (gamma_max / gamma_table)**(1 - particle_slope))
-    f[np.isnan(f)] = 0
-    particle_distribution[i] += f
+        f = 0 * gamma_table
+        f = (gamma_cool_1[i] < gamma_table) * (gamma_table < gamma_max) * (1 -
+            (gamma_max / gamma_table)**(1 - particle_slope))
+        f[np.isnan(f)] = 0
+        particle_distribution[i] += f
 
-    f = 0 * gamma_table
-    f = (gamma_cool_1[i] < gamma_table) * (gamma_table < gamma_min) * ((1 -
-        (gamma_max / gamma_min)**(1 - particle_slope)) * (gamma_min / gamma_table)**(1 - particle_slope))
-    f[np.isnan(f)] = 0
-    particle_distribution[i] += f
+        f = 0 * gamma_table
+        f = (gamma_cool_1[i] < gamma_table) * (gamma_table < gamma_min) * ((1 -
+            (gamma_max / gamma_min)**(1 - particle_slope)) * (gamma_min /
+            gamma_table)**(1 - particle_slope))
+        f[np.isnan(f)] = 0
+        particle_distribution[i] += f
 
-    f = 0 * gamma_table
-    f = (gamma_cool_2[i] < gamma_table) * (gamma_table < gamma_min) * ((
-        gamma_min / gamma_table)**(1 - particle_slope) - (1 - b *
-        timestable[i] * gamma_table)**(particle_slope - 1))
-    f[np.isnan(f)] = 0
-    particle_distribution[i] += f
+        f = 0 * gamma_table
+        f = (gamma_cool_2[i] < gamma_table) * (gamma_table < gamma_min) * ((
+            gamma_min / gamma_table)**(1 - particle_slope) - (1 - b *
+            timestable[i] * gamma_table)**(particle_slope - 1))
+        f[np.isnan(f)] = 0
+        particle_distribution[i] += f
 
-    particle_distribution[i] *= (particle_distribution_norm * gamma_table**(
-        -1 - particle_slope) / (b * (particle_slope - 1)))
+        particle_distribution[i] *= (particle_distribution_norm *
+            gamma_table**(-1 - particle_slope) / (b * (particle_slope - 1)))
+
+    # For times, above the pericenter time, we first interpolate the particle
+    # distribution at the pericenter time and then compute the updated
+    # particle distribution with the new Lorentz factor following eqn. 9 in
+    # Christie et al. 2017.
+    else:
+        f = 0 * gamma_table
+        interp_particle_distribution_tp = interp1d(np.log(gamma_table),
+            particle_distribution[np.sum(timestable <= pericenter_time) - 1])
+        gamma_large_tp = 1 - b * gamma_table * (timestable[i] -
+            pericenter_time)
+        f = interp_particle_distribution_tp(np.log(
+            gamma_table / gamma_large_tp)) * gamma_large_tp**(-2)
+        f[np.isnan(f)] = 0.
+        particle_distribution[i] = f
+
 ###############################################################
 ###############################################################
 
@@ -184,12 +217,13 @@ for i in range(np.shape(timestable)[0]):
 ##############################################################
 ##############################################################
 
-#################### Plots of Particle Distribution & Synchrotron Power ####################
-############################################################################################
+####### Plots of Particle Distribution & Synchrotron Spectrum #######
+#####################################################################
 
 fig, axs = plt.subplots(2, 1)
 for i in range(np.shape(timestable)[0]):
     axs[0].loglog(gamma_table, particle_distribution[i])
+    axs[0].set_xlim([10**1., 10**10.])
 axs[0].set_xlabel(r'$\gamma$')
 axs[0].set_ylabel(r'N$^e$($\gamma , t$)')
 
@@ -197,7 +231,7 @@ for i in range(np.shape(timestable)[0]):
     axs[1].loglog(photon_frequency, synchrotron_pow[i])
     axs[1].set_xlim([10**7., 10**26.])
     axs[1].set_ylim([10**21., 10**36.])
-#axs[1].loglog(1.4*(10**14.), 6*1.15*(10**34.), 'bx')
+# axs[1].loglog(1.4*(10**14.), 6*1.15*(10**34.), 'bx')
 axs[1].loglog(10**sgr_a_observations_frequency,
     10**sgr_a_observations_luminosity, 'x', color = 'black')
 axs[1].set_xlabel(r'$\nu$ (Hz)')
@@ -205,5 +239,5 @@ axs[1].set_ylabel(r'$\nu \, L_\nu$ (erg/s)')
 
 fig.tight_layout()
 plt.show()
-############################################################################################
-############################################################################################
+#####################################################################
+#####################################################################
