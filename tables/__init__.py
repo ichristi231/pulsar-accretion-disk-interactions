5239b3802aa0920968bf2b2af257401e67660f25
Ian Christie <ichristi231@gmail.com>
2019-09-09 11:51:56 -0500

    added another file for importing

--- tables/__init__.py
+++ tables/__init__.py
@@ -2,5 +2,7 @@ from pathlib import Path
 import numpy as np
 
 tables_dir = Path.cwd() / "tables"
-sgr_a_observations = np.loadtxt(
-    tables_dir / "sgr_a_observations.txt")
+sgr_a_observations_frequency = np.loadtxt(
+    tables_dir / "sgr_a_observations_frequency.txt")
+sgr_a_observations_luminosity = np.loadtxt(
+    tables_dir / "sgr_a_observations_luminosity.txt")
