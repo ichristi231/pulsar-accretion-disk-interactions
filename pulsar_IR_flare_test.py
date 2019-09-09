2bd6e559192f04de634116eb8111c4daf7c59b86
Ian Christie <ichristi231@gmail.com>
2019-09-09 13:06:44 -0500

    added more documentation

--- pulsar_IR_flare_test.py
+++ pulsar_IR_flare_test.py
@@ -30,7 +30,7 @@ electric_charge = 4.8032068 * (10**-10.)
 ##########################################################
 
 # Spin-down luminosity of the pulsar in erg/seconds.
-spin_down_L = 10**35.
+spin_down_L = 3.*10**35.
 
 # Slope of injected particle distribution (assumed to be a power-law).
 particle_slope = 2.2
@@ -40,7 +40,7 @@ epsilon_e = 0.1
 epsilon_B = 0.1
 
 # Pericenter distance of the pulsar and Bondi Radius in cm.
-pericenter_dist = 10**(15.)
+pericenter_dist = 5.*10**(16.)
 Bondi_radius = 10**17.
 
 # Pericenter time, i.e. the time it takes the pulsar to traverse
@@ -56,13 +56,13 @@ Bondi_num_den = 10**2
 
 # Magnetic field strength at the pericenter distance in Gauss.
 # Note that we follow the parametric scalings provided in the
-# paper.
+# Christie et al. 2017.
 pericenter_mag_field = 0.007 * np.sqrt((epsilon_B / 0.1) * (Bondi_num_den / 100.) * (Bondi_radius / (10**17.))) * (pericenter_dist / (10**16.))**(-1)
 # print("Magnetic field strength: " + str(pericenter_mag_field) + " G. \n")
 
 
 # Minimum Lorentz factor of the injected particle distribution.
-gamma_min = 10**2.
+gamma_min = 10**3.
 
 print("\nPericenter time is " + str(pericenter_time) + " s = " + str(pericenter_time / 3600.) + " hr.\n")
 
@@ -82,13 +82,15 @@ gamma_max = 1.4 * (10**9.) * ((epsilon_B / 0.1) * (Bondi_num_den / 100.) *
 gamma_table = np.logspace(0, np.log10(gamma_max), 99)
 
 # Normalization factor of the injected particle distribution (see eqn. 6).
-particle_distribution_norm = epsilon_e * spin_down_L * (particle_slope - 2) / (electron_mass
-	* speed_light**2 * (gamma_min**(2 - particle_slope) - gamma_max**(2 - particle_slope)))
+particle_distribution_norm = epsilon_e * spin_down_L * (particle_slope - 2) / (
+    electron_mass * speed_light**2 * (gamma_min**(2 - particle_slope) -
+        gamma_max**(2 - particle_slope)))
 
 # Factor b defined just after eqn. 5 in the paper.
 b = pericenter_mag_field**2 * thomson_cross_section / (6 * np.pi * electron_mass * speed_light)
 
-# Cooling Lorentz factors gc1(t) and gc2(t), defined following eqn. 8.
+# Cooling Lorentz factors gc1(t) and gc2(t), defined following eqn. 8 in
+# Christie et al. 2016.
 gamma_cool_1 = gamma_max / (1 + timestable * b * gamma_max)
 gamma_cool_2 = gamma_min / (1 + b * timestable * gamma_min)
 
@@ -102,26 +104,32 @@ f = 0 * gamma_table
 # provided in eqn. 8, and add it to the existing particle distibution. Note
 # that they are listed in the same order as eqn. 8.
 for i in range(np.shape(timestable)[0]):
-    f = (gamma_table > gamma_min) * (gamma_table < gamma_cool_1[i]) * (1 - (1 - b * gamma_table * timestable[i])**(particle_slope - 1))
+    f = (gamma_table > gamma_min) * (gamma_table < gamma_cool_1[i]) * (1 - (1 -
+    b * gamma_table * timestable[i])**(particle_slope - 1))
     f[np.isnan(f)] = 0
     particle_distribution[i] += f
 
     f = 0 * gamma_table
-    f = (gamma_cool_1[i] < gamma_table) * (gamma_table < gamma_max) * (1 - (gamma_max / gamma_table)**(1 - particle_slope))
+    f = (gamma_cool_1[i] < gamma_table) * (gamma_table < gamma_max) * (1 -
+        (gamma_max / gamma_table)**(1 - particle_slope))
     f[np.isnan(f)] = 0
     particle_distribution[i] += f
 
     f = 0 * gamma_table
-    f = (gamma_cool_1[i] < gamma_table) * (gamma_table < gamma_min) * ((1 - (gamma_max / gamma_min)**(1 - particle_slope)) * (gamma_min / gamma_table)**(1 - particle_slope))
+    f = (gamma_cool_1[i] < gamma_table) * (gamma_table < gamma_min) * ((1 -
+        (gamma_max / gamma_min)**(1 - particle_slope)) * (gamma_min / gamma_table)**(1 - particle_slope))
     f[np.isnan(f)] = 0
     particle_distribution[i] += f
 
     f = 0 * gamma_table
-    f = (gamma_cool_2[i] < gamma_table) * (gamma_table < gamma_min) * ((gamma_min / gamma_table)**(1 - particle_slope) - (1 - b * timestable[i] * gamma_table)**(particle_slope - 1))
+    f = (gamma_cool_2[i] < gamma_table) * (gamma_table < gamma_min) * ((
+        gamma_min / gamma_table)**(1 - particle_slope) - (1 - b *
+        timestable[i] * gamma_table)**(particle_slope - 1))
     f[np.isnan(f)] = 0
     particle_distribution[i] += f
 
-    particle_distribution[i] *= (particle_distribution_norm * gamma_table**(-1 - particle_slope) / (b * (particle_slope - 1)))
+    particle_distribution[i] *= (particle_distribution_norm * gamma_table**(
+        -1 - particle_slope) / (b * (particle_slope - 1)))
 ###############################################################
 ###############################################################
 
@@ -135,7 +143,8 @@ for i in range(np.shape(timestable)[0]):
 x_store = np.logspace(-20, 2, 99)
 f_store = 0 * x_store
 for i in range(np.shape(x_store)[0]):
-    f_store[i] = x_store[i] * integrate.quad(lambda y: np.exp(y) * kv(5./3., np.exp(y)), np.log(x_store[i]), np.log(10**5.))[0]
+    f_store[i] = x_store[i] * integrate.quad(lambda y: np.exp(y) * kv(5./3.,
+        np.exp(y)), np.log(x_store[i]), np.log(10**5.))[0]
 f_interp = interp1d(np.log10(x_store), np.log10(f_store))
 
 # Table of photon frequency values in Hertz.
@@ -155,17 +164,20 @@ for i in range(np.shape(photon_frequency)[0]):
     for j in range(np.shape(gamma_table)[0]):
 
         if (x[i, j] >= 10**-20.) * (x[i, j] <= 100.) == 1:
-            p_single[i, j] = (np.sqrt(3.) * (electric_charge**3.) * pericenter_mag_field / (electron_mass
+            p_single[i, j] = (np.sqrt(3.) * (electric_charge**3.) *
+                pericenter_mag_field / (electron_mass
             	* (speed_light**2.))) * (10**f_interp(np.log10(x[i, j])))
             pass
         pass
     pass
 
 # Synchrotron power in erg/seconds.
-synchrotron_pow = np.zeros((np.shape(timestable)[0], np.shape(photon_frequency)[0]))
+synchrotron_pow = np.zeros((np.shape(timestable)[0],
+    np.shape(photon_frequency)[0]))
 for i in range(np.shape(timestable)[0]):
     for j in range(np.shape(photon_frequency)[0]):
-        integrand = gamma_table * (np.log(gamma_table[1]) - np.log(gamma_table[0])) * p_single[j] * particle_distribution[i]
+        integrand = gamma_table * (np.log(gamma_table[1]) -
+            np.log(gamma_table[0])) * p_single[j] * particle_distribution[i]
         synchrotron_pow[i, j] = photon_frequency[j]*np.sum(integrand)
         pass
     pass
