099bab82c34dba370587fec9b25108368c8f75fe
Ian Christie <ichristi231@gmail.com>
2019-09-09 16:58:31 -0500

    added xray observations to tables directory

--- tables/__init__.py
+++ tables/__init__.py
@@ -2,7 +2,11 @@ from pathlib import Path
 import numpy as np
 
 tables_dir = Path.cwd() / "tables"
-sgr_a_observations_frequency = np.loadtxt(
-    tables_dir / "sgr_a_observations_frequency.txt")
-sgr_a_observations_luminosity = np.loadtxt(
-    tables_dir / "sgr_a_observations_luminosity.txt")
+sgr_a_observations_radio_frequency = np.loadtxt(
+    tables_dir / "sgr_a_observations_radio_frequency.txt")
+sgr_a_observations_radio_luminosity = np.loadtxt(
+    tables_dir / "sgr_a_observations_radio_luminosity.txt")
+sgr_a_observations_xray_frequency = np.loadtxt(
+    tables_dir / "sgr_a_observations_xray_frequency.txt")
+sgr_a_observations_xray_luminosity = np.loadtxt(
+    tables_dir / "sgr_a_observations_xray_luminosity.txt")
