0dfa424dae0cf0f91da35a9dd2704fd06957976d
Ian Christie <ichristi231@gmail.com>
2019-09-27 01:01:11 -0700

    added read in for new text files for single particle synchrotron emissivity

--- tables/__init__.py
+++ tables/__init__.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import numpy as np
 
 tables_dir = Path.cwd() / "tables"
+
 sgr_a_observations_radio_frequency = np.loadtxt(
     tables_dir / "sgr_a_observations_radio_frequency.txt")
 sgr_a_observations_radio_luminosity = np.loadtxt(
@@ -16,3 +17,11 @@ sgr_a_observations_IR_frequency = np.loadtxt(
     tables_dir / "sgr_a_observations_IR_frequency.txt")
 sgr_a_observations_IR_luminosity = np.loadtxt(
     tables_dir / "sgr_a_observations_IR_luminosity.txt")
+
+antf_spin_down_luminosities = np.loadtxt(
+    tables_dir / "ANTF_spin_down_luminosity_pulsar_catalog.txt")
+
+synchrotron_log10_x = np.loadtxt(
+    tables_dir / "single_part_emissivity_x_store.txt")
+synchrotron_log10_f_x = np.loadtxt(
+    tables_dir / "single_part_emissivity_f_store.txt")
